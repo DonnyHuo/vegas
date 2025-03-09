20378fc1683933f4d168cbb144b292e4e83f4603
DonnyHuo <huocheng122828@gmail.com>
2025-03-09 21:44:10 +0800

    feat: add v2

--- src/http/request.js
+++ src/http/request.js
@@ -1,7 +1,12 @@
 import { request } from "graphql-request";
 
+import { store } from "../store";
+
 export const fetchData = async (data) => {
+  const version = store.getState().version;
   const endpoint =
-    "https://api.studio.thegraph.com/query/84515/stakingreward/version/latest";
+    version === 2
+      ? "https://api.studio.thegraph.com/query/84515/stakingrewardv2/v0.0.1"
+      : "https://api.studio.thegraph.com/query/84515/stakingreward/version/latest";
   return request(endpoint, data);
 };
