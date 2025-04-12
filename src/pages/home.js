e1639699c66705ca085577cf30295480779e4c2c
DonnyHuo <huocheng122828@gmail.com>
2025-04-12 22:27:07 +0800

    feat: debug

--- src/pages/home.js
+++ src/pages/home.js
@@ -894,7 +894,7 @@ const Home = () => {
         >
           <div className="p-[20px] text-center text-[14px]">
             <p className="font-bold mb-2">
-              {version === 2 ? t("switch") : t("switchv3")}
+              {version === 2 ? t("switch") : t("switchV3")}
             </p>
             <p>{t("invitationDetected")}</p>
             <p className="mt-2">{t("confirmToBind")}</p>
