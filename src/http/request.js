0d1653811ac4f171717d3b9d1bfff6e438f9ab89
DonnyHuo <huocheng122828@gmail.com>
2025-04-16 14:58:27 +0800

    feat:change url

--- src/http/request.js
+++ src/http/request.js
@@ -6,9 +6,11 @@ export const fetchData = async (data) => {
   const version = store.getState().version;
   const endpoint =
     version === 2
-      ? "https://api.studio.thegraph.com/query/84515/stakingrewardv2/version/latest"
+      ? "https://gateway.thegraph.com/api/subgraphs/id/H47Rbtgf4wSXUYSXJCQghojAW2PVwHdgBht41ukXnN5x"
       : version === 3
-      ? "https://api.studio.thegraph.com/query/84515/stakingrewardv3/version/latest"
+      ? "https://gateway.thegraph.com/api/subgraphs/id/A6resAWaXr7koffpoqWmqfbqtg4QLtVBN2otxNC5dfk"
       : "https://api.studio.thegraph.com/query/84515/stakingreward/version/latest";
-  return request(endpoint, data);
+  return request(endpoint, data, undefined, {
+    Authorization: "Bearer 15e21550a1f515dd302fe54bf5635a40"
+  });
 };
